fd83439fb9767e18d6668b15626972475539efb5
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-10 16:04:29 +0500

    Update functions.php

--- raising-fse/functions.php
+++ raising-fse/functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Raising  FSE functions and definitions
- * tyugghfddsdwrdgtufgg4
+ * fgfd
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package Raising  FSE
